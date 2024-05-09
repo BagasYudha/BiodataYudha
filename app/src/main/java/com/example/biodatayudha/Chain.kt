559de1e3ee6313a633a0cc9ae4bd3a4cef9a9d97
Bagas Yudha <bayud.kematian@gmail.com>
2024-05-09 22:08:01 +0700

    Mencoba Chain dan Guidelines

---
+++ app/src/main/java/com/example/biodatayudha/Chain.kt
@@ -0,0 +1,11 @@
+package com.example.biodatayudha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chain)
+    }
+}
